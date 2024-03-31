3f13a29697122c10d7cfa79adf7bb02830b7cdda
Mehakhasiyan <165031075+Mehakhasiyan@users.noreply.github.com>
2024-03-31 18:37:00 +0530

    Add files via upload

---
+++ hangman-gui/README.md
@@ -0,0 +1,63 @@
+## **Hangman Game**
+
+Hangman is a classic word guessing game known around the world. I took my own approach to building this addictive game using the Python programming language. The game uses [http://random-word-api.herokuapp.com/homean](http://random-word-api.herokuapp.com/home). The API contains more than 170,000 words to guess, and a “live” image of a hangman is displayed as the player makes incorrect guesses. After completing a round, the program will also ask if the user wants to find the meaning of a guessed (or not) word.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes or just pure fun.
+
+## Prerequisites
+
+*   You will need Python 3 installed on your system: [https://www.python.org/](https://www.python.org/)
+*   You will also need to install the required libraries:  
+    `pip install pysimplegui`  
+    `pip install requests`
+*   Alternatively, run this command for automatic module installation:  
+    `pip install -r requirements.txt`
+*   You will need the `Roboto Mono.ttf` font, which can be found in the main folder.
+
+## Installing
+
+Clone the repository and change to the project directory  
+`$ git clone https://github.com/paichiwo/Hangman_GUI.git`  
+`$ cd Hangman GUI`  
+`$ .\Hangman.py`
+
+## Customization
+
+`Hangman.psd` file that contains all Hangman's art is included in the `img` folder.  
+`font_used` can be changed to any desired font from your OS.  
+`psg.theme` can also be changed to any desired color. [List of themes](https://media.geeksforgeeks.org/wp-content/uploads/20200511200254/f19.jpg).
+
+![](screenshot/hang0.png)
+
+## How to Play
+
+The computer chooses a secret word, and the player has to guess it one letter at a time.
+
+The player can guess one letter at a time. If the guessed letter is in the word, it will be revealed in its correct position. If the letter's not in the word, the player loses a life.
+
+The game continues until the player has guessed the word, or until the player has run out of 10 lives.
+
+The game will add one point for each correctly guessed word.
+
+When the round completes, the game will ask if the user wishes to find the meaning of the word.
+
+## **Contributing**
+
+Contributions are always welcome and appreciated! If you find any issues or have suggestions for improvements, please feel free to open an issue or pull request on the GitHub repository.
+
+To contribute, please follow these steps:
+
+1.  Fork the repository to your own GitHub account.
+2.  Clone the repository to your local machine.
+3.  Create a new branch for your changes.
+4.  Make your changes and commit them with clear commit messages.
+5.  Push your changes to your forked repository.
+6.  Open a pull request to the main repository.
+
+Please ensure that your code is well-documented, tested, and follows the existing code style and guidelines. By contributing, you agree to release your code under the same MIT license as the original code.
+
+## Authors
+
+Mehak Hasiyan - [https://github.com/Mehakhasiyan](https://github.com/Mehakhasiyan)
---
+++ hangman-gui/src/helpers.py
@@ -0,0 +1,123 @@
+import json
+import os
+import random
+import sys
+import requests
+from deep_translator import GoogleTranslator
+
+
+def resource_path(relative_path):
+    """PyInstaller requirement,
+    Get an absolute path to resource, works for dev and for PyInstaller."""
+    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+def load_settings():
+    """Load settings from .json file, if key not found, default language settings will be 'EN'"""
+
+    with open(resource_path('../data/settings.json'), 'r') as settings_file:
+        settings = json.load(settings_file)
+        return settings.get('language', 'EN')
+
+
+def save_settings(settings_dict):
+    """Save settings to .json file"""
+
+    with open(resource_path('../data/settings.json'), 'w') as settings_file:
+        json.dump(settings_dict, settings_file)
+
+
+def load_high_scores():
+    """Load high scores from the .json file"""
+
+    with open(resource_path('../data/high_scores.json'), 'r') as file:
+        scores = json.load(file)
+    return scores
+
+
+def update_high_scores(name, score):
+    """Update high scores in the .json file"""
+
+    scores = load_high_scores()
+
+    # If there are already three high scores and the new score is lower than the lowest score, return
+    if len(scores) >= 3 and score <= min(scores.values()):
+        return
+    # Update the high scores with the new score
+    scores[name] = score
+    # Keep only the top three scores
+    top_scores = dict(sorted(scores.items(), key=lambda x: x[1], reverse=True)[:3])
+    # Save the updated scores to the file
+    with open(resource_path('../data/high_scores.json'), 'w') as file:
+        json.dump(top_scores, file)
+
+
+def create_high_scores_string():
+    """Create a string to display on the splash screen"""
+
+    high_scores = load_high_scores()
+    high_scores_string = ""
+    for name, score in high_scores.items():
+        high_scores_string += f"{name}  {score}\n"
+    return high_scores_string
+
+
+def translate_eng_to_pol(word):
+    """Translate any given word to Polish"""
+
+    my_translator = GoogleTranslator(source='english', target='polish')
+    result = my_translator.translate(text=word)
+    return result
+
+
+def secret_word_api():
+    """Get secret word from API, and choose only words <= 12 characters long"""
+
+    url = "https://random-word-api.herokuapp.com/word"
+    response = requests.get(url)
+    text = response.json()
+    word = text[0]
+    if len(word) <= 12:
+        return word
+    else:
+        return secret_word_api()
+
+
+def secret_word_from_list():
+    """Get word from word_list.json"""
+
+    with open('../data/word_list.json', 'r') as json_file:
+        json_data = json_file.read()
+    word_dict = json.loads(json_data)
+    word_list = word_dict['word_list']
+
+    return word_list
+
+
+def word_api_or_word_list(language):
+    """Get secret word from wordlist.py if the API fails for any reason"""
+
+    try:
+        word = secret_word_api()
+    except (requests.ConnectionError, requests.HTTPError, requests.exceptions.JSONDecodeError):
+        word = random.choice(secret_word_from_list())
+
+    if language == 'EN':
+        return word.upper()
+    elif language == 'PL':
+        print(word)
+        return translate_eng_to_pol(word).upper().split(" ")[0]
+
+
+def check_word_meaning(word, language):
+    """Create a link to check the meaning of the secret word"""
+
+    if language == "EN":
+        query = word.lower().replace(" ", "+")
+        url = f'https://www.google.com/search?q={query}+meaning'
+        return url
+    elif language == "PL":
+        query = word.lower().replace(" ", "+")
+        url = f'https://www.google.com/search?q={query}+znaczenie'
+        return url
